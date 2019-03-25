c2154a6d1957d3e977fdb54c6f9c01806a5a5ad2
maxgunter99 <maxgunter99@gmail.com>
2019-03-25 18:12:09 -0500

    minimum requirements met

--- instagram/instagram/src/Components/dataLayout.js
+++ instagram/instagram/src/Components/dataLayout.js
@@ -1,14 +1,31 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
 
 function PostContainer(props) {
     return (
         <>
+            <div className='instagram'>
+                <p><strong>Instagram</strong></p>
+            </div>
             {props.dummyData.map(dummyData => (
                 <div key={dummyData.id} className="posts">
-                    <img src={dummyData.thumbnailUrl} alt={dummyData.username} />
-                    <p>{dummyData.username}</p>
-                    <img src={dummyData.imageUrl} alt={dummyData.username} />
+                    {/* username and profile image */}
+
+                    <div className='top'>
+                        <img className='profile' src={dummyData.thumbnailUrl} alt={dummyData.username} />
+                        <p><strong>{dummyData.username}</strong></p></div>
+                    <img className='picture' src={dummyData.imageUrl} alt={dummyData.username} />
+
+                    {/* likes */}
+                    <p className='likes'>{dummyData.likes} Likes</p>
+
+                    {/* comments */}
+                    <div className='comments'>
+                        {dummyData.comments &&
+                            dummyData.comments.map(e => <p  key={e.id}> <strong>{e.username}</strong> {e.text}</p>)}
+                            <p className='timestamp'>{dummyData.timestamp}</p>
+                    </div>
                 </div>
             ))}
         </>
@@ -17,12 +34,22 @@ function PostContainer(props) {
   
 PostContainer.propTypes = {
     posts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number,
-        username: PropTypes.string,
-        thumbnailUrl: PropTypes.string,
+        PropTypes.shape({
+            id: PropTypes.number,
+            username: PropTypes.string,
+            thumbnailUrl: PropTypes.string,
+            likes: PropTypes.number,
+            comments: PropTypes.arrayOf(
+                PropTypes.shape({
+                  id: PropTypes.string.isRequired,
+                  username: PropTypes.string.isRequired,
+                  text: PropTypes.string.isRequired
+                })
+            ),
+            timestamp: PropTypes.string,
 
-      })
+        })
     )
 };
+
 export default PostContainer;
