802ac2d39e378f3bbeb557fa7227908466d0afc7
maxgunter99 <maxgunter99@gmail.com>
2019-03-25 21:14:31 -0500

    instagram almost pixel perfect,comenting is not fully functional though

--- instagram/instagram/src/Components/dataLayout.js
+++ instagram/instagram/src/Components/dataLayout.js
@@ -1,30 +1,65 @@
 import React from 'react';
-
 import PropTypes from 'prop-types';
+import logo from './src/InstagramLogo.png';
+import instagram from './src/InstagramText.png';
+import compass from './src/compass.png';
+import heart from './src/heart.png';
+import profile from './src/profile.png';
+import comment from './src/comment.png';
+import bookmark from './src/bookmark.png';
+import share from './src/share.png';
+
 
 function PostContainer(props) {
     return (
         <>
             <div className='instagram'>
-                <p><strong>Instagram</strong></p>
+                <div className='logo'>
+                    <img src={logo} />
+                    <img src={instagram} />
+                </div>
+                <input></input>
+                <div className='connect'>
+                    <img src={compass} />
+                    <img src={heart} />
+                    <img src={profile} />
+                </div>
             </div>
+            <p className='even'></p>
             {props.dummyData.map(dummyData => (
                 <div key={dummyData.id} className="posts">
                     {/* username and profile image */}
 
                     <div className='top'>
-                        <img className='profile' src={dummyData.thumbnailUrl} alt={dummyData.username} />
-                        <p><strong>{dummyData.username}</strong></p></div>
+                        <div classname='user'>
+                            <img className='profile' src={dummyData.thumbnailUrl} alt={dummyData.username} />
+                            <p><strong>{dummyData.username}</strong></p>
+                        </div>
+
+                        <strong>...</strong>
+                        
+                    </div>
                     <img className='picture' src={dummyData.imageUrl} alt={dummyData.username} />
 
                     {/* likes */}
-                    <p className='likes'>{dummyData.likes} Likes</p>
+                    <div class='likes'>
+                        <div>
+                            <img className='heart'src={heart} />
+                            <img className='comment'src={comment} />
+                            <img className='share'src={share} />
+                        </div>
+                        <div className='book'>
+                            <img className='bookmark'src={bookmark} />
+                        </div>
+                    </div>
+                    <p className='likes'><strong>{dummyData.likes} Likes</strong></p>
 
                     {/* comments */}
                     <div className='comments'>
                         {dummyData.comments &&
                             dummyData.comments.map(e => <p  key={e.id}> <strong>{e.username}</strong> {e.text}</p>)}
                             <p className='timestamp'>{dummyData.timestamp}</p>
+                            <input></input>
                     </div>
                 </div>
             ))}
